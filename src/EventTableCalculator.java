0cb0e30c89b7576017fcda8376318e29962a6e3b
Leolinson <135208930+Leolinson@users.noreply.github.com>
2023-09-25 12:29:55 +0200

    Foldername updated

--- src/EventTableCalculator.java
+++ src/EventTableCalculator.java
@@ -4,6 +4,7 @@ public class EventTableCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
         System.out.print("Enter the number of groups: ");
         int numGroups = scanner.nextInt();
 
